90c9e5594053a35ad1cff841647e2a605d33650f
hoah27293-droid <hoah27293@gmail.com>
2026-07-27 06:35:24 +0800

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,74 +1,37 @@
-const menu = [
-
-
-{
-category:"🍚 Cơm",
-name:"Cơm ba chỉ cháy cạnh",
-price:"260 ₱"
-},
-
-
-{
-category:"🍚 Cơm",
-name:"Cơm sườn nướng",
-price:"280 ₱"
-},
-
-
-{
-category:"🍚 Cơm",
-name:"Cơm gà xối mỡ",
-price:"240 ₱"
-},
-
-
-{
-category:"🍜 Phở",
-name:"Phở bò đặc biệt",
-price:"280 ₱"
-},
-
-
-{
-category:"🍜 Phở",
-name:"Phở gà truyền thống",
-price:"250 ₱"
-},
-
-
-{
-category:"🥘 Món Việt",
-name:"Bánh xèo miền Tây",
-price:"250 ₱"
-},
-
-
-{
-category:"🥘 Món Việt",
-name:"Nem rán Hà Nội",
-price:"180 ₱"
-},
-
-
-{
-category:"🥩 Món nướng",
-name:"Bò nướng BBQ",
-price:"450 ₱"
-},
-
-
-{
-category:"🦐 Hải sản",
-name:"Tôm hấp sả",
-price:"380 ₱"
-},
-
-
-{
-category:"🥤 Đồ uống",
-name:"Trà đào cam sả",
-price:"120 ₱"
-}
-
-
-];
+[
+  {
+    "category": "Cơm",
+    "name": "Cơm ba chỉ cháy cạnh",
+    "price": "260"
+  },
+  {
+    "category": "Cơm",
+    "name": "Cơm chả lá lốt",
+    "price": "260"
+  },
+  {
+    "category": "Cơm",
+    "name": "Cơm thịt kho trứng",
+    "price": "260"
+  },
+  {
+    "category": "Cơm",
+    "name": "Cơm cá kho",
+    "price": "260"
+  },
+  {
+    "category": "Cơm",
+    "name": "Cơm gà rang gừng",
+    "price": "260"
+  },
+  {
+    "category": "Cơm",
+    "name": "Cơm gà chiên mắm",
+    "price": "260"
+  },
+  {
+    "category": "Cơm",
+    "name": "Cơm gà xối mỡ",
+    "price": "260"
+  }
+]
