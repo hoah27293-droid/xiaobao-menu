d402aa8cb0203fbf2340e95148437ec72719f2ea
hoah27293-droid <hoah27293@gmail.com>
2026-07-27 06:20:12 +0800

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,31 @@
+const menu = [
+    {
+        category: "🍚 Cơm",
+        name: "Cơm ba chỉ cháy cạnh",
+        price: "260 ₱"
+    },
+
+    {
+        category: "🍚 Cơm",
+        name: "Cơm sườn nướng",
+        price: "280 ₱"
+    },
+
+    {
+        category: "🍚 Cơm",
+        name: "Cơm gà xối mỡ",
+        price: "240 ₱"
+    },
+
+    {
+        category: "🍜 Phở",
+        name: "Phở bò đặc biệt",
+        price: "280 ₱"
+    },
+
+    {
+        category: "🍜 Phở",
+        name: "Phở gà truyền thống",
+        price: "250 ₱"
+    }
+];
